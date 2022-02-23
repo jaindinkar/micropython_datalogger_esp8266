5c124239486e377f66b336beb9b9c47e3a0a5b88
Dinkar Jain <jain.dinkar675@gmail.com>
2022-02-23 11:00:27 +0530

    sd support added

--- src/main.py
+++ src/main.py
@@ -1,15 +1,31 @@
-from machine import Pin, I2C
+import os
+from time import sleep, sleep_ms
+from machine import Pin, I2C, SPI
 from onewire import OneWire
-from time import sleep
-import urequests
+# import micropython
 
 from ds18x20 import DS18X20
 from ssd1306 import SSD1306_I2C
 from libs.bme280_sm import BME280
 from libs.scd30_sm import SCD30
+from libs.sdcard import SDCard
 
 import modules.net_connect as net_connect
-from config.zapier import zap_webhook_url
+
+
+# Pin Assignment
+p_i2c_sda = Pin(4) # D2
+p_i2c_scl = Pin(5) # D1
+p_hspi_en = Pin(15) # D8
+p_one_wire = Pin(0) # D3 Flash button might not work. 
+# SPI will take GPIO 12,13,14 | D6,D7,D5 automatically.
+
+# ESP8266 I2C Pin assignment
+i2c = I2C(scl=p_i2c_scl, sda=p_i2c_sda)
+# ESP8266 One Wire Pin assignment
+one_wire = OneWire(p_one_wire)
+# ESP8266 SPI Pin assignment
+hspi = SPI(1)
 
 
 # Availablity check:
@@ -17,33 +33,36 @@ is_available_ds18 = False
 is_available_bme280 = False
 is_available_ssd1306 = False
 is_available_scd30 = False
+is_available_sd = False
 
-# Unit constants
-unit_humi = '%RH'
-unit_temp = 'degC'
-unit_pres = 'mBar'
-unit_co2 = 'ppm'
 
-# Initial Sensor values:
-str_temp_ds18 = None
-str_temp_bme280 = None
-str_humi_bme280 = None
-str_pres_bme280 = None
-str_temp_scd30 = None
-str_humi_scd30 = None
-str_co2_scd30 = None
+print()  # First space.
 
-# Update frequency (Seconds):
-update_freq = 5
+# SPI sd card config:
+try:
+  os.umount('/sdmc')
+  print('Unmounted old SD card on /sdmc path.')
+except BaseException as e:
+  print(f"No previous SD card mount point found. Error: {e}")
 
-# ESP8266 I2C Pin assignment
-i2c = I2C(scl=Pin(5), sda=Pin(4))
-# ESP8266 One Wire Pin assignment
-one_wire = OneWire(Pin(13))
+# print(micropython.mem_info(1))
+try:
+  sd = SDCard(hspi, p_hspi_en)
+  print('Found SD card.')
+  vfs = os.VfsFat(sd)
+  print('SD card VfsFat obj created.')
+  os.mount(vfs, '/sdmc')
+  # os.mount(sd, '/sdmc')
+  print(os.listdir("/sdmc"))
+  os.umount('/sdmc')
+  print('Mounted SD card on /sdmc path.')
+  is_available_sd = True
+except BaseException as e:
+  print(f"SD card error. Error: {e}")
+
 
 # Print available I2C devices:
-print()  # First space.
-print(f"Available I2C addresses: {list(map(hex, i2c.scan()))}")
+# print(f"Available I2C addresses: {list(map(hex, i2c.scan()))}")
 
 # One-wire DS18B20 config:
 try:
@@ -55,9 +74,10 @@ try:
   else:
     print('No DS18B sensor present on assigned pin.')
 except BaseException as e:
-  print("Could not found any DS18B sensor on assigned pin.")
+  print(f"Could not found any DS18B sensor on assigned pin. Error: {e}")
 
 # I2C OLED Config:
+sleep_ms(300)
 try:
   oled_width = 128
   oled_height = 64
@@ -65,25 +85,25 @@ try:
   print('Found SSD 1306 OLED Display.')
   is_available_ssd1306 = True
 except BaseException as e:
-  print('Could not found any SSD1306 OLED on I2C bus.')
+  print(f'Could not found any SSD1306 OLED on I2C bus. Error: {e}')
 
 # I2C BME280 Config:
+sleep_ms(300)
 try:
   bme = BME280(i2c=i2c)
   print('Found BME 280 Sensor.')
   is_available_bme280 = True
 except BaseException as e:
-  print('Could not found any BME280 Sensor on I2C bus.')
-  print(e)
+  print(f'Could not found any BME280 Sensor on I2C bus. Error: {e}')
 
 # I2C SCD30 Config:
+sleep_ms(300)
 try:
   scd30 = SCD30(i2c, 0x61)
   print('Found SCD30 Sensor.')
   is_available_scd30 = True
 except BaseException as e:
-  print('Could not found any SCD30 Sensor on I2C bus.')
-  print(e)
+  print(f'Could not found any SCD30 Sensor on I2C bus. Error: {e}')
 
 
 # Function for updating OLED Screen
@@ -96,6 +116,12 @@ def updateScreen(
   str_temp_scd30='N/A',
   str_co2_scd30='N/A'
 ):
+  # Unit constants
+  unit_humi = '%RH'
+  unit_temp = 'degC'
+  unit_pres = 'mBar'
+  unit_co2 = 'ppm'
+
   oled.fill(0)
   oled.text(
       f"BH-{'N/A' if str_humi_bme280 is None else str_humi_bme280} {unit_humi}", 0, 0)
@@ -117,28 +143,46 @@ def updateScreen(
   oled.show()
 
 
-def connectingToInternetScreen():
-  oled.fill(0)
-  oled.text('Connecting to', 0, 20)
-  oled.text('Internet...', 3, 30)
-  oled.show()
+# def connectingToInternetScreen():
+#   oled.fill(0)
+#   oled.text('Connecting to', 0, 20)
+#   oled.text('Internet...', 3, 30)
+#   oled.show()
 
 
 # Connecting to internet:
-if is_available_ssd1306:
-  connectingToInternetScreen()
+# if is_available_ssd1306:
+#   connectingToInternetScreen()
 net_connect.connect()
-
+gc.collect()
 # Screen Test
 # updateScreen()
+
+
+# Initial Sensor values:
+str_temp_ds18 = None
+str_temp_bme280 = None
+str_humi_bme280 = None
+str_pres_bme280 = None
+str_temp_scd30 = None
+str_humi_scd30 = None
+str_co2_scd30 = None
+
+# Update frequency (Seconds):
+update_freq = 5
+
 
 while True:
   # Reading DS18B20 Sensor
   if is_available_ds18:
     ds_sensor.convert_temp()
-    temp_ds18 = ds_sensor.read_temp(roms[0])
-    str_temp_ds18 = f"{temp_ds18:.2f}"
-    print(f"DS18B reading: Temperature:{str_temp_ds18}")
+    for rom in roms:
+      try:
+        temp_ds18 = ds_sensor.read_temp(rom)
+        str_temp_ds18 = f"{temp_ds18:.2f}"
+        print(f"DS18B reading for rom: {rom}: Temperature:{str_temp_ds18}")
+      except BaseException as e:
+        print("Could not found any DS18B sensor on assigned pin.")
 
   # Reading BME280 Sensor
   if is_available_bme280:
@@ -173,28 +217,6 @@ while True:
       str_temp_scd30=str_temp_scd30,
       str_co2_scd30=str_co2_scd30,
     )
-
-  # Release the webhook to log data to google sheets:
-  try:
-    payload = {
-      "temp_bme280": str_temp_bme280,
-      "humi_bme280": str_humi_bme280,
-      "pres_bme280": str_pres_bme280,
-      "temp_ds18b": str_temp_ds18
-    }
-
-    request_headers = {'Content-Type': 'application/json'}
-
-    request = urequests.post(
-      zap_webhook_url,
-      json=payload,
-      headers=request_headers
-    )
-    print(f"Post Response status: {request.status_code} {request.reason}")
-    request.close()
-
-  except BaseException as e:
-    print('Failed to log sensor readings online.')
 
   print('-----------------------------------------------------------')
   # Takes atleast 1ms to clean garbage objects created,
