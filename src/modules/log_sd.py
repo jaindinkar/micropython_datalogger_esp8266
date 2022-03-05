792982d778a10628cb2e6d5cb5ffede5ee0932b5
Dinkar Jain <jain.dinkar675@gmail.com>
2022-03-05 15:01:12 +0530

    SD access robustness ++

--- src/modules/log_sd.py
+++ src/modules/log_sd.py
@@ -14,55 +14,77 @@ class Storage:
         self.spi_en = spi_en
         self.mnt_pt = mnt_pt
         self.f_path = f_path
-        self.init_storage()
+        if not self.__init_storage():
+            raise Exception("Storage Initialization Failed.")
 
-    def init_storage(self):
-        self.unmount_storage()
+    def __init_storage(self):
+        if not self.__unmount_storage():
+            print(f"No previous mount point found for SD card: Path:{self.mnt_pt}")
         try:
             self.sd = SDCard(self.spi, self.spi_en)
             self.vfs = os.VfsFat(self.sd)
         except BaseException as e:
             print(f"[SD INIT ERROR] SD card: Path:{self.mnt_pt} Error: {e}")
+            return(False)
+        else:
+            return(True)
         
         # os.mount(self.vfs, self.mnt_pt)
         # os.mount(slef.sd, self.mnt_pt)
         # print(micropython.mem_info(1))
 
-    def mount_storage(self):
+    def __mount_storage(self):
         try:
             os.mount(self.vfs, self.mnt_pt)
         except BaseException as e:
-            print(f"[MOUNT ERROR] SD card: Path:{self.mnt_pt} Error: {e}")
+            print(f"[SD MOUNT ERROR] SD card: Path:{self.mnt_pt} Error: {e}")
+            return(False)
+        else:
+            return(True)
 
-    def unmount_storage(self):
+    def __unmount_storage(self):
         try:
             os.umount(self.mnt_pt)
         except BaseException as e:
-            print(f"[UNMOUNT ERROR] SD card: Path:{self.mnt_pt} Error: {e}")
-
-    def storage_content(self):
-        self.mount_storage()
-        try:
-            dir_list = os.listdir(self.mnt_pt)
-        except BaseException as e:
-            return(f"[LIST ERROR] SD Card: Error retreiving directory List! Error: {e}")
+            print(f"[SD UNMOUNT ERROR] SD card: Path:{self.mnt_pt} Error: {e}")
+            return(False)
         else:
-            return(dir_list)
-        finally:
-            self.unmount_storage()
+            return(True)
+
+    def print_storage_content(self):
+        if self.__mount_storage():
+            try:
+                # os.listdir is memory hungry!
+                # files = os.listdir(self.mnt_pt)
+                files_iterator = os.ilistdir(self.mnt_pt)
+            except BaseException as e:
+                print(f"[SD LIST ERROR] SD Card: Error retreiving directory List! Error: {e}")
+            else:
+                print("File List Below ===> []. Empty dir won't print anything.")
+                for file in files_iterator:
+                    print(file)
+            finally:
+                self.__unmount_storage()
+        else:
+            raise Exception("Can't mount the storage block.")
 
     def write_data(self, *data_pts):
         # Comma separated values in log.
         # Order is determined by pos of arguments.
-        self.mount_storage()
-        try:
-            with open(f"{self.mnt_pt}{self.f_path}", "a") as f:
-                f.write(f"{','.join(data_pts)}\n")
-                f.flush()
-        except OSError:
-            print(f"Log file not found! Creating a new {self.f_path[1:]} file.")
-            with open(f'{self.mnt_pt}{self.f_path}', "x+") as f:
-                f.write(f"{','.join(data_pts)}\n")
-                f.flush()
-        finally:
-            self.unmount_storage()
+        if self.__mount_storage():
+            try:
+                with open(f"{self.mnt_pt}{self.f_path}", "a") as f:
+                    f.write(f"{','.join(data_pts)}\n")
+                    f.flush()
+            except BaseException as e:
+                print(f"[SD FILE APPEND ERROR] Assuming file not found, creating a new {self.f_path[1:]} file. Error: {e}")
+                try:
+                    with open(f'{self.mnt_pt}{self.f_path}', "x+") as f:
+                        f.write(f"{','.join(data_pts)}\n")
+                        f.flush()
+                except BaseException as e:
+                    print(f"[SD FILE WRITE ERROR] SD Card: File creation and write Failed. Error: {e}")
+            finally:
+                self.__unmount_storage()
+        else:
+            raise Exception("Can't mount the storage block.")
